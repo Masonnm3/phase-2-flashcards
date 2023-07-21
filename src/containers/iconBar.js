4d807f9ccae3d66c5481e88e3a7bd8d4472ba406
iskandervulto <iskanderhyvulto@gmail.com>
2023-07-21 01:00:50 -0400

    route part 2

--- src/containers/iconBar.js
+++ src/containers/iconBar.js
@@ -1,21 +1,23 @@
-import { RiMenuLine, RiHome4Line, RiBook2Line } from "react-icons/ri";
-import { GiMagnifyingGlass, } from "react-icons/gi";
+import { RiMenuLine, RiHome4Line, RiBook2Line, RiInformationLine, RiPhoneLine } from "react-icons/ri";
+import { GiMagnifyingGlass  } from "react-icons/gi";
+import { Link } from 'react-router-dom'
 
 
 export default function IconBar({ setQuizMode, setAddQuestionsView }) {
   return (
     <div className="sidebar-icon-bar">
-      <RiHome4Line
-        className="sidebar-icon"
-        onClick={() => {
-          setQuizMode(false);
-          setAddQuestionsView(false);
-        }}
-      />
-      <RiMenuLine className="sidebar-icon" />
-      <GiMagnifyingGlass className="sidebar-icon" />
-      <RiBook2Line className="sidebar-icon" />
-      
-    </div>
+    <Link to='/'>
+        <RiHome4Line className="sidebar-icon" />
+    </Link>
+        <RiMenuLine className="sidebar-icon" />
+        <GiMagnifyingGlass className="sidebar-icon" />
+        <RiBook2Line className="sidebar-icon" />
+    <Link to="/AboutPage">
+        <RiInformationLine className="sidebar-icon" />
+    </Link>
+    <Link to="/Contact">
+        <RiPhoneLine className="sidebar-icon" />
+    </Link>
+  </div>
   );
 }
